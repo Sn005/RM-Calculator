c4867ce7be6444a119d83ff70f16714259152185
snasna <wisperinthedustbox@gmail.com>
2018-07-14 18:00:22 +0900

    test追加

---
+++ components/organisms/RmDashboard/partial/RmResult/RmResultItem/index.spec.js
@@ -0,0 +1,26 @@
+import { mount } from '@vue/test-utils'
+import RmResultItem from './RmResultItem.vue'
+
+describe('RmResultItem.vue', () => {
+  it('RM係数のテスト', () => {
+    const props = {
+      propsData: {
+        maxWeight: 100,
+        rm: 8
+      }
+    }
+    const wrapper = mount(RmResultItem, props)
+    console.log(wrapper.vm.ratio)
+    expect(wrapper.vm.ratio).toBe(1.2)
+  })
+  it('RM計算結果のテスト', () => {
+    const props = {
+      propsData: {
+        maxWeight: 100,
+        rm: 10
+      }
+    }
+    const wrapper = mount(RmResultItem, props)
+    expect(wrapper.vm.result).toBe(80)
+  })
+})
